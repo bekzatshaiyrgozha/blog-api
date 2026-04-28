5e344cb18217b8c0275b41cbc867e137beea32ec
Bekzat <b_shaiyrgozha@kbtu.kz>
2026-04-28 19:11:10 +0500

    Fix: import DebugConsumer in notifications.routing

--- apps/notifications/routing.py
+++ apps/notifications/routing.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .consumers import CommentConsumer
+from .consumers import CommentConsumer, DebugConsumer
 
 websocket_urlpatterns = [
     path("ws/posts/<slug:slug>/comments/", CommentConsumer.as_asgi()),
